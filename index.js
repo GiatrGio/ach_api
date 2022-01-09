332f84acb7a3eb5236007eac111865459438300f
Georgios Giatrakis <georgiosgiatrakis@Georgioss-MacBook-Air.local>
2022-01-09 13:00:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+var cors = require('cors')
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+app.use(express.json());
+app.use(cors())
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+
+const upload = multer({ storage: storage });
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/auth", authRoute);
+app.use("/user", userRoute);
+app.use("/post", postRoute);
+app.use("/category", categoryRoute);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(5002, () => {
+  console.log("Backend is running.");
+});
